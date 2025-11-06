6a25e5142d2b6c50f955a43cfbed2c1293d31fb3
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-11-06 10:40:53 -0500

    databases are working now

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,17 @@
 -- TODO
+DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS folders;
+
+
+CREATE TABLE folders(
+    id serial PRIMARY KEY, 
+    name text UNIQUE NOT NULL
+);
+
+CREATE TABLE files(
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    size int NOT NULL,
+    folder_id integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+    UNIQUE (folder_id, name)
+)
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seed();
@@ -7,4 +8,27 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  for (let i = 0; i < 3; i++) {
+    const folderNane = `Folder ${i}`;
+    const {
+      rows: [folder],
+    } = await db.query(
+      `
+      INSERT INTO folders(name) 
+      VALUES ($1) 
+      RETURNING id`,
+      [folderNane]
+    );
+    for (let j = 0; j < 5; j++) {
+      const fileName = `Person ${i} -File ${j}`;
+      const fileSize = faker.number.int({ max: 100 });
+
+      await db.query(
+        `
+      INSERT INTO files(name,size,folder_id)
+      VALUES($1,$2,$3)`,
+        [fileName, fileSize, folder.id]
+      );
+    }
+  }
 }
