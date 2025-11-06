49f1a28d8337865e1c6470e45538f6c840d6f50f
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-11-06 18:07:16 -0500

    this took me all day!

--- api/folders.js
+++ api/folders.js
@@ -1,11 +1,41 @@
 import express from "express";
-import { getFolders } from "#db/queries/folders";
+import { createFile, getFolderById, getFolders } from "#db/queries/folders";
 
 const foldersRouter = express.Router();
 
 foldersRouter.route("/").get(async (req, res) => {
   const folders = await getFolders();
   res.send(folders);
+});
+
+foldersRouter.param("id", async (req, res, next, id) => {
+  const folder = await getFolderById(id);
+
+  if (!folder) {
+    return res.status(404).send("Folder not found");
+  }
+  req.folder = folder;
+  next();
+});
+
+foldersRouter.route("/:id").get((req, res) => {
+  res.send(req.folder);
+});
+
+foldersRouter.route("/:id/files").post(async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body required.");
+  }
+  if (!req.body.name || !req.body.size) {
+    return res.status(400).send("Body is missing required fields");
+  }
+  const file = await createFile({
+    name: req.body.name,
+    size: req.body.size,
+    folder_id: Number(req.params.id),
+  });
+
+  res.status(201).send(file);
 });
 
 export default foldersRouter;
--- db/queries/folders.js
+++ db/queries/folders.js
@@ -9,3 +9,31 @@ export async function getFolders() {
 
   return folders;
 }
+
+export async function getFolderById(id) {
+  const sql = `
+    SELECT folders.*,
+    (SELECT json_agg(files) 
+     FROM files 
+     WHERE files.folder_id=folders.id) as files
+  FROM folders
+  WHERE id = $1
+    `;
+  const {
+    rows: [folder],
+  } = await db.query(sql, [id]);
+  return folder;
+}
+
+export async function createFile({ name, size, folder_id }) {
+  const sql = `
+     INSERT INTO files (name, size, folder_id)
+     VALUES ($1,$2, $3)
+     RETURNING *
+    `;
+  const {
+    rows: [file],
+  } = await db.query(sql, [name, size, folder_id]);
+
+  return file;
+}
